748c817194a316254810096203af665503f8fc84
PlatformTeam <platform@red.local>
2021-10-12 17:57:04 +0900

    feat: google 소셜 로그인 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,22 @@
+import { GetServerSidePropsContext } from 'next';
 import React from 'react'
 import 'tailwindcss/tailwind.css'
-import SampleComponent from '../components/SampleComponent'
+import SampleComponent from '../components/SampleComponent';
+import SignIn from './SignIn';
 
-function HomePage() {
-  return <SampleComponent />
+function HomePage({...props}) {
+  return (
+    <>
+      <SignIn />
+      <SampleComponent />
+    </>
+  )
 }
 
-export default HomePage
+export default HomePage;
+
+export const getServerSideProps = async ({ query, req }: GetServerSidePropsContext) => {
+  return {
+    props: {},
+  };
+};
